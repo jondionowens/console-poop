04a35768efe5e9764c1b5ef6f92cec9a33eb147a
Jonathan Owens"git config --global user.email jo@jondionowens.comgit config --global user.name 'Jonathan Owens <jo@jondionowens.com>
2019-04-09 12:21:36 -0700

    Changed verion requirements

--- extension.js
+++ extension.js
@@ -1,8 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
-const fs = require('fs');
-const path = require('path');
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 
@@ -18,8 +17,8 @@ function activate(context) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
+	console.log(vscode.window)
 	let disposable = vscode.commands.registerCommand('extension.insert-poop', function () {
-		console.log(vscode.window)
 	})
 
 	context.subscriptions.push(disposable);
